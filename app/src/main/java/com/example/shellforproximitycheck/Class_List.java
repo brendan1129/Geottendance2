f042bd59d2f8847356dc32163350956f0c8a7820
Kailey <krkravabloski@gmail.com>
2021-11-04 02:28:52 -0400

    Added Class List Activity Created Database Entities with Room

---
+++ app/src/main/java/com/example/shellforproximitycheck/Class_List.java
@@ -0,0 +1,14 @@
+package com.example.shellforproximitycheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Class_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_list);
+    }
+}
